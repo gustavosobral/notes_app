e2876fc437524ae056e84bdb3e560922e1daa059
Gustavo Sobral <ghsobral@gmail.com>
2016-07-30 16:27:26 -0300

    Add textAngular, bootstrap and FA icons

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ $ npm install -g webpack
 $ npm install -g webpack-dev-server
 $ npm install
 ```
+## Development
+
+Run `$ npm run serve` to starts the webpack-dev-server. Then, access him on: http://localhost:8080/
+
+## Production
+
+Execute the following command to build the production files on bin/ folder.
+
+```bash
+$ npm run build
+```
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-var angular   = require('angular');
-var uiRouter  = require('angular-ui-router');
+var angular         = require('angular');
+var uiRouter        = require('angular-ui-router');
+var textAngular     = require('textangular');
 
 var home    = require('home');
 var note    = require('note');
 
 var Routes   = require('./routes.js');
 
+require('textangular/dist/textAngular-sanitize.min');
 var app = angular.module('notesApp', [
-  home, note, uiRouter
+  home, note, uiRouter, textAngular
 ]);
 
 app.config(Routes);
