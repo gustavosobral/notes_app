476207a5f7d98b61565f0a6fbc958d39b1e9e27e
Gustavo Sobral <ghsobral@gmail.com>
2016-07-31 17:54:53 -0300

    Add notes index API integration

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -4,12 +4,31 @@ function HomeController($log, NotesappService) {
 
   initialize();
 
+  // Controller initailization
   function initialize() {
+    getNotes();
+  }
+
+  function getNotes() {
     NotesappService.getNotes()
       .then(function(notes) {
         vm.notes = notes;
+
+        // Parse note status into Font Awesome icons
+        for (var i = notes.length - 1; i >= 0; i--) {
+          switch(notes[i].status) {
+            case 'active':
+              vm.notes[i].faClass = 'fa-bolt';
+              break;
+            case 'inactive':
+              vm.notes[i].faClass = 'fa-ban';
+              break;
+            case 'draft':
+              vm.notes[i].faClass = 'fa-pencil';
+          }          
+        }
     });
-  }  
+  }
 }
 
 module.exports = HomeController;
